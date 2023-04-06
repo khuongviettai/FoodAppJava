ada4c230b63542e765286523c351867751aa679a
Khương Viết Tài <khuongtai@MacBook-Pro.local>
2023-04-06 19:48:46 +0700

    first commit

---
+++ app/src/main/java/com/example/foodorder/constant/Constant.java
@@ -0,0 +1,23 @@
+package com.example.foodorder.constant;
+
+public interface Constant {
+
+    String GENERIC_ERROR = "General error, please try again later";
+
+
+    String LINK_FACEBOOK = "https://www.facebook.com/khuongviettai";
+
+    String PHONE_NUMBER = "+84945034118";
+    String GMAIL = "khuongtai789@gmail.com";
+
+    String ZALO_LINK = "https://zalo.me/0945034118";
+
+    String FIREBASE_URL = "https://foododer-7e2c6-default-rtdb.firebaseio.com";
+
+    String CURRENCY = " 000 VNĐ";
+    int TYPE_PAYMENT_CASH = 1;
+    String PAYMENT_METHOD_CASH = "Tiền mặt";
+
+    // Key Intent
+    String KEY_INTENT_FOOD_OBJECT = "food_object";
+}
